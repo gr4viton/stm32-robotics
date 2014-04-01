341da3e71b12caa7b6dc59e398e10efb6070c0fb
gr4viton <lordmutty@gmail.com>
2014-04-01 20:21:08 +0200

    common.mk - makefile - added standard c99 and gnu++11

--- mk/common.mk
+++ mk/common.mk
@@ -1,7 +1,7 @@
 ###############################################################################
 # C flags
 
-CFLAGS	+= -O0 -g
+CFLAGS	+= -Og -g -std=gnu99
 CFLAGS	+= -Wextra -Wshadow -Wimplicit-function-declaration
 CFLAGS	+= -Wredundant-decls -Wmissing-prototypes -Wstrict-prototypes
 CFLAGS	+= -ffunction-sections -fdata-sections -ffast-math -fno-common
@@ -11,10 +11,12 @@ CFLAGS	+= -Wmissing-declarations -Wmissing-include-dirs -Wunreachable-code
 ###############################################################################
 # C++ flags
 
-CXXFLAGS	+= -O0 -g
-CXXFLAGS	+= -Wextra -Wshadow -Wredundant-decls  -Weffc++
+CXXFLAGS	+= -Og -g -std=gnu++11
+CXXFLAGS	+= -Wextra -Wshadow -Wredundant-decls
+# -Weffc++
 CXXFLAGS	+= -fno-common -ffunction-sections -fdata-sections
 CXXFLAGS	+= -fno-exceptions -fno-unwind-tables
+CXXFLAGS	+= -std=gnu++11
 
 ###############################################################################
 # C & C++ preprocessor common flags
@@ -33,7 +35,16 @@ LDFLAGS	+= --static -nostartfiles
 LDFLAGS	+= -Wl,--gc-sections
 #LDFLAGS += -Wl,--no-warn-mismatch
 
+# newlib nano - small printf
+    LDFLAGS	+= --specs=nano.specs
+# to print floating point numbers
+    LDFLAGS += -u _printf_float
+# to scan floating point numbers
+#    LDFLAGS += -u _scanf_float
+
 ###############################################################################
 # Linker libraries to be built with
 
 LDLIBS	+= -Wl,--start-group -lc -lgcc -lnosys -Wl,--end-group
+
+
